306d1be76b7f68fc108d8d06e5e410041931a015
Kapp-M <kalp.mehta05@gmail.com>
2024-09-18 12:29:19 -0400

    Parsing Data and storing Student Data objects into vector.

--- Assignment-2/Source.cpp
+++ Assignment-2/Source.cpp
@@ -1,3 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct STUDENT_DATA {
+    string firstName;
+    string lastName;
+};
+
 int main() {
-	return 1;
+    return 1;
+    vector<STUDENT_DATA> students;
+    ifstream file(" StudentData.txt ");
+    string line;
+
+    while (getline(file, line)) {
+        size_t commaPos = line.find(',');
+        STUDENT_DATA student;
+        student.firstName = line.substr(0, commaPos);
+        student.lastName = line.substr(commaPos + 1);
+        students.push_back(student);
+    }
+    file.close();
+
+    return 0;
 }
